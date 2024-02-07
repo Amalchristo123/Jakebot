efe77420cabb9436a516a79c99d91f99c569460e
AnshVachhani <142148746+AnshVachhani@users.noreply.github.com>
2024-02-07 09:33:18 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1744,7 +1744,7 @@ async def auto_filter(client, msg, spoll=False):
         ENABLE_SHORTLINK = settings['is_shortlink']
     else:
         await save_group_settings(message.chat.id, 'is_shortlink', False)
-        ENABLE_SHORTLINK = False®
+        ENABLE_SHORTLINK = False
         
     pre = 'filep' if settings['file_secure'] else 'file'
     if ENABLE_SHORTLINK and settings["button"]:
