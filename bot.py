94e7d800d1d1c9b743fe014f308871dabd481ba9
Amalchristo123 <139036853+Amalchristo123@users.noreply.github.com>
2024-02-12 09:44:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            workers=200,
+            workers=250,
             plugins={"root": "plugins"},
             sleep_threshold=10,
         )
